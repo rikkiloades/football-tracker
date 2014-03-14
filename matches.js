32ab87ed892c747cde35d8d36b553d561d6c48f6
Rikki Loades <rikki1234@gmail.com>
2014-03-14 14:28:10 +0000

    Adds latest match data.

--- matches.js
+++ matches.js
@@ -890,5 +890,36 @@ App.matches = [
                 ]
             }
         ]
+    },
+    {
+        "id": 32,
+        "date": "2014-03-7 18:00:00",
+        "pitch": "A",
+        "teams": [
+            {
+                "name": "Kamakazee Rockets",
+                "score": 5,
+                "players": [
+                    "Rikki",
+                    "Lewis",
+                    "Rob",
+                    "Josh",
+                    "Kirstie",
+                    "Ryan"
+                ]
+            },
+            {
+                "name": "Consistently inconsistent",
+                "score": 4,
+                "players": [
+                    "Emma",
+                    "Jason",
+                    "Jon",
+                    "Stephen",
+                    "Darren",
+                    "Oli"
+                ]
+            }
+        ]
     }
 ];
