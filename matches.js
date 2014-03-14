9e95514de2f7c8958f7203b63f58271e3eccd182
Rikki Loades <rikki1234@gmail.com>
2014-03-14 20:56:12 +0000

    Adds latest match data.

--- matches.js
+++ matches.js
@@ -893,7 +893,7 @@ App.matches = [
     },
     {
         "id": 32,
-        "date": "2014-03-7 18:00:00",
+        "date": "2014-03-07 18:00:00",
         "pitch": "A",
         "teams": [
             {
@@ -918,6 +918,35 @@ App.matches = [
                     "Stephen",
                     "Darren",
                     "Oli"
+                ]
+            }
+        ]
+    },
+    {
+        "id": 33,
+        "date": "2014-03-14 18:00:00",
+        "pitch": "A",
+        "teams": [
+            {
+                "name": "Blue",
+                "score": 8,
+                "players": [
+                    "Rikki",
+                    "Lewis",
+                    "Darren",
+                    "Josh",
+                    "Kirstie"
+                ]
+            },
+            {
+                "name": "Red",
+                "score": 7,
+                "players": [
+                    "Emma",
+                    "Rob",
+                    "Jon",
+                    "Russell",
+                    "Cedric"
                 ]
             }
         ]
